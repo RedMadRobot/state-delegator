d921e16fd9fec8e43c690dfbf392e741f73bdfc5
Alexander Egorov <avegrv@gmail.com>
2018-12-17 01:04:29 +0400

    [Sample] Change Loading Activity sample

--- sample/src/main/java/com/redmadrobot/loading/LoadingActivity.kt
+++ sample/src/main/java/com/redmadrobot/loading/LoadingActivity.kt
@@ -1,7 +1,6 @@
 package com.redmadrobot.loading
 
 import android.os.Bundle
-import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.redmadrobot.R
 import com.redmadrobot.StubStateData
@@ -10,41 +9,25 @@ import kotlinx.android.synthetic.main.activity_loading.*
 
 class LoadingActivity : AppCompatActivity() {
 
-    private var handler = Handler()
-
     private lateinit var screenState: LoadingStateDelegate
-
-    private val showLoadingTask = { screenState.showLoading() }
-    private val showZeroScreenTask = { screenState.showStub() }
-    private val showZeroWithStubScreenTask = {
-        screenState.showStub(StubStateData(
-                iconResId = R.mipmap.ic_launcher,
-                titleResId = R.string.loading_zero_title,
-                descriptionResId = R.string.loading_zero_description
-        ))
-    }
-    private val showContentTask = { screenState.showContent() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loading)
         screenState = LoadingStateDelegate(loading_content, loading_progress, loading_zero_stub)
-    }
 
-    override fun onResume() {
-        super.onResume()
-        handler.post(showLoadingTask)
-        handler.postDelayed(showZeroScreenTask, 3_000L)
-        handler.postDelayed(showLoadingTask, 6_000L)
-        handler.postDelayed(showContentTask, 9_000L)
-        handler.postDelayed(showZeroWithStubScreenTask, 12_000L)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        handler.removeCallbacks(showLoadingTask)
-        handler.removeCallbacks(showZeroScreenTask)
-        handler.removeCallbacks(showContentTask)
-        handler.removeCallbacks(showZeroWithStubScreenTask)
+        loading_show_loading_btn.setOnClickListener {
+            screenState.showLoading()
+        }
+        loading_show_content_btn.setOnClickListener {
+            screenState.showContent()
+        }
+        loading_show_stub_btn.setOnClickListener {
+            screenState.showStub(StubStateData(
+                    iconResId = R.mipmap.ic_launcher,
+                    titleResId = R.string.loading_zero_title,
+                    descriptionResId = R.string.loading_zero_description
+            ))
+        }
     }
 }
