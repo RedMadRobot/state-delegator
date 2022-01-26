a61eecb994dc8a6cee5a83b85bd9853fe7ca30e9
Osip Fatkullin <o.fatkullin@redmadrobot.com>
2022-01-26 13:49:49 +0300

    Configure publication to OSSRH

--- state-delegator/build.gradle.kts
+++ state-delegator/build.gradle.kts
@@ -1,6 +1,29 @@
+import com.redmadrobot.build.dsl.*
+
 plugins {
     id("com.redmadrobot.android-library")
     id("com.redmadrobot.publish")
+}
+
+group = "com.redmadrobot"
+description = "Collection of classes that helps you to manage a screen state"
+
+redmadrobot {
+    publishing {
+        signArtifacts.set(true)
+
+        pom {
+            setGitHubProject("RedMadRobot/state-delegator")
+
+            licenses {
+                mit()
+            }
+
+            developers {
+                developer(id = "avegrv", name = "Alexander Egorov", email = "av.egorov@redmadrobot.com")
+            }
+        }
+    }
 }
 
 kotlin {
@@ -12,4 +35,10 @@ dependencies {
 
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.robolectric:robolectric:3.1.1")
+}
+
+publishing {
+    repositories {
+        if (credentialsExist("ossrh")) ossrh()
+    }
 }
